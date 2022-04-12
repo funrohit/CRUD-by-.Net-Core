3c053226013d46a6a09cf6e6fa84d4d257ad17d7
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-04-12 15:15:01 +0530

    Add files via upload

---
+++ EmpModel.cs
@@ -0,0 +1,10 @@
+﻿namespace mycore.Models
+{
+    public class EmpModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
